3505077bd95d362b99632c7ae79db1c3c7ac87e1
ivan4322 <ivancho13codigo@gmail.com>
2023-08-01 19:26:46 -0500

    subiendo

---
+++ index.js
@@ -0,0 +1,10 @@
+let names = "ivan ";
+let lastName = "aristizabal";
+const yearofBirth = "septiembre/30/1994";
+
+let city = "medellin ";
+
+
+console.log ("mi nombre es:" , names , "mi apellido es",lastName, "mi año de nacimiento es :" ,yearofBirth, "vivo en la ciudad de " ,city);
+
+
